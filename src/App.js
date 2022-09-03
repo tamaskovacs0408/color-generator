b07b78cef8cbea3e4b7ca6437d0f4a169b10a986
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-03 11:21:59 +0200

    Create form

--- src/App.js
+++ src/App.js
@@ -9,12 +9,22 @@ function App() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-  }
+  };
 
   return (
     <div className="App">
       <h1>Color Generator</h1>
-      <div className="form__container"></div>
+      <div className="form__container">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(event) => setColor(event.target.value)}
+          />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+      <div className="color__container"></div>
     </div>
   );
 }
