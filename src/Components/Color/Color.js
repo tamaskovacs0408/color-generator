cb853e8606c9c0d024fd8beaa417084309e4379a
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-03 17:48:22 +0200

    Styling components

--- src/Components/Color/Color.js
+++ src/Components/Color/Color.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import RGBtoHEX from '../RgbToHex/RgbToHex.js';
+import RGBtoHEX from "../RgbToHex/RgbToHex.js";
 
 const Color = ({ rgb, weight, type, hexColor }) => {
   const [copyinfo, setCopyinfo] = useState(false);
@@ -7,12 +7,19 @@ const Color = ({ rgb, weight, type, hexColor }) => {
   const hexValue = `#${hexColor}`; // Set this to the inline style backgroundColor
 
   return (
-    <div className={`color ${(type === 'tint' ? 'tint' : 'shade')} `} style={{ backgroundColor: `${hexValue}` }}>
+    <div
+      className={`color ${type === "tint" ? "tint" : "shade"} `}
+      style={{ backgroundColor: `${hexValue}` }}
+      onClick={() => {
+        setCopyinfo(true);
+        navigator.clipboard.writeText(hex);
+      }}
+    >
       <span className="percent_value">{weight}%</span>
       <span className="color_value">{hex}</span>
+      {copyinfo && <span className="copied"> HEX code copied</span>}
     </div>
   );
 };
 
 export default Color;
- // ${index > 10 && 'type-tint'}
