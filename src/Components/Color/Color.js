8c92f07c8a690ec60421aa9ef91bde3c9eb76839
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-03 16:53:35 +0200

    Style Color component

--- src/Components/Color/Color.js
+++ src/Components/Color/Color.js
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from "react";
 import RGBtoHEX from '../RgbToHex/RgbToHex.js';
 
-const Color = ({ rgb, weight, index, hexColor }) => {
+const Color = ({ rgb, weight, index, type, hexColor }) => {
   const [copyinfo, setCopyinfo] = useState(false);
   //const bgColor = rgb.join(","); // the rgb value for the background color
   const hex = RGBtoHEX(...rgb);
   const hexValue = `#${hexColor}`;
 
   return (
-    <div className={`color`} style={{ backgroundColor: `${hexValue}` }}>
+    <div className={`color ${(type === 'tint' ? 'shade' : 'tint')} `} style={{ backgroundColor: `${hexValue}` }}>
       <p className="percent_value">{weight}%</p>
-      <p className="color_value">{hexValue}</p>
+      <p className="color_value">{hex}</p>
     </div>
   );
 };
 
 export default Color;
+ // ${index > 10 && 'type-tint'}
