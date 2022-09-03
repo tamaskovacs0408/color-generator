41c7c23d1f641a72f880a1522798e0ce956f1ceb
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-09-03 19:11:46 +0200

    Update styles

--- src/App.js
+++ src/App.js
@@ -22,24 +22,25 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Tint and Shade Generator</h1>
-      <div className="form__container">
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={color}
-            onChange={(event) => setColor(event.target.value)}
-            placeholder="Enter your HEX color"
-            className={`${error ? "error" : null}`}
-          />
-          <input type="submit" value="Submit" />
-        </form>
+      <div className="App-header">
+        <h1>Tint and Shade Generator</h1>
+        <div className="form__container">
+          <form onSubmit={handleSubmit}>
+            <input
+              id="input_field"
+              type="text"
+              value={color}
+              onChange={(event) => setColor(event.target.value)}
+              placeholder="Enter your HEX color"
+              className={`${error ? "error" : null}`}
+            />
+            <input className="btn" type="submit" value="Submit" />
+          </form>
+        </div>
       </div>
       <div className="color__container">
         {colorList.map((color) => {
-          return (
-            <Color key={uuid()} {...color} hexColor={color.hex} />
-          );
+          return <Color key={uuid()} {...color} hexColor={color.hex} />;
         })}
       </div>
     </div>
